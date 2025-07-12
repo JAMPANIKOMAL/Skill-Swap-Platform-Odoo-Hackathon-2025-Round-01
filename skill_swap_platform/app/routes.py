85fb32b81e4ebb10956a7650b816a9eca8c48ff0
JAMPANI <jampanikomal2005@gmail.com>
2025-07-12 11:42:48 +0530

    update

--- skill_swap_platform/app/routes.py
+++ skill_swap_platform/app/routes.py
@@ -2,6 +2,9 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+
+
 from .models import User, Skill
 from .forms import RegisterForm, LoginForm, SkillForm
 from . import db
